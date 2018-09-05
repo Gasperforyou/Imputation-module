1a7f884c93c291404bc95f683b719bdcccdfa322
gasperjelovcan <gasper.jelovcan5@gmail.com>
2018-09-05 10:56:31 +0200

    scVi

--- owimpute.py
+++ owimpute.py
@@ -3,15 +3,24 @@ import numpy
 from PyQt5 import QtCore, QtGui
 import Orange.data
 from Orange.data import Table
-import scvi
+
+from subprocess import call
+
+import os
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+
+from scvi.dataset import CsvDataset, BreastCancerDataset, BrainSmallDataset, CortexDataset
+
+from scvi.models import *
+from scvi.inference import UnsupervisedTrainer
 
 
 from Orange.widgets.widget import OWWidget, Input, Output, settings
 from Orange.widgets import gui
 
 from Orange.preprocess import Impute, Average
-
-
 
 
 class Impute(OWWidget):
@@ -36,14 +45,16 @@ class Impute(OWWidget):
     Commit = "Impute"
     train = []
     estimate = []
-    functions = {0: "True", 1: "self.average()"}
+    compute = []
+    path = ""
+    functions = {0: "flow", 1: "self.average()", 2: "self.median()", 3:"self.WMean_chisquared()", 4:"self.scvi()"}
     gene = ""
 
-    def __init__(self):
+    def __init__(self, pat):
         super().__init__()
-
+        self.path = pat
         self.listbox = gui.listBox(self.controlArea, self, "imputation_method", "methods" , box = "Imputation method")
-        self.methods = ["Flow through", "Average"]
+        self.methods = ["Flow through", "Average", "Median", "WMean_chisquared", "scVi"]
         self.imputation_method = gui.ControlledList([0], self.listbox)
         gui.button(self.controlArea, self, "Impute", callback=self.comm)
 
@@ -52,10 +63,9 @@ class Impute(OWWidget):
     def set_data(self, dataset):
         if dataset is not None:
             self.dataset = dataset
-            self.selection()
         else:
             self.dataset = None
-        self.commit()
+        #self.commit()
 
     def comm(self):
         self.selection()
@@ -64,51 +74,110 @@ class Impute(OWWidget):
     def selection(self):
         if self.dataset is None:
             return
-        self.impute()
+        if(self.imputation_method[0]<=3):
+            self.impute()
+        else:
+            func = self.functions[self.imputation_method[0]]
+            eval(func)
+            print("Done!")
+
 
     def commit(self):
         if self.dataset is None:
             return
         self.Outputs.sample.send(self.dataset)
-
+        Orange.data.Table.save(self.dataset, "output.csv")
+        return
 
     def impute(self):
-        for dom in self.dataset.domain.variables:
-            self.estimate = [d for d in range(len(self.dataset)) if self.dataset[d][dom] == 0]
-            totrain =  [d for d in range(len(self.dataset)) if (d not in self.estimate) ]
-            self.train = Orange.data.Table.from_table_rows(self.dataset, totrain)
-            if(len(self.train)==0):
+        func = self.functions[self.imputation_method[0]]
+        if(func=="flow"):
+            return
+        self.compute = numpy.copy(self.dataset.X)
+        for dom in range(self.compute.shape[1]):
+            self.estimate = numpy.where(self.compute[:,dom] == 0)
+            self.train = self.compute[numpy.where(self.compute[:,dom] != 0)]
+            if(self.train.shape[0]==0):
                 continue
             self.gene = dom
-            func = self.functions[self.imputation_method[0]]
             eval(func)
+        print("Done!")
+        return
 
 
     def average(self):
-        avg = numpy.mean([d[self.gene] for d in self.train])
-
-        for d in self.estimate:
+        avg = numpy.mean(self.train[:,self.gene])
+        for d in self.estimate[0]:
             self.dataset[d][self.gene] = avg
 
+    def median(self):
+        med = numpy.median(self.train[:,self.gene])
+        for d in self.estimate[0]:
+            self.dataset[d][self.gene] = med
 
-    def main(argv=None):
-        from AnyQt.QtWidgets import QApplication
-        # PyQt changes argv list in-place
-        app = QApplication(list(argv) if argv else [])
-        argv = app.arguments()
+    def WMean_chisquared(self):
+        avg, sum_weights = numpy.average(self.train[:,self.gene], weights=numpy.random.chisquare(3, self.train.shape[0]), returned=True)
+        for d in self.estimate[0]:
+            self.dataset[d][self.gene] = avg
 
-        ow = OWDataSamplerA()
-        ow.show()
-        ow.raise_()
+    def scvi(self):
+        #datasetBreastCancer = BreastCancerDataset()
+        #datasetBrainSmall = BrainSmallDataset()
+        dataset = CortexDataset()
 
-        dataset = Orange.data.Table("ccp_data_Tcells_normCounts.counts.cycle_genes.tab")
-        ow.set_data(dataset)
-        ow.handleNewSignals()
-        app.exec_()
-        ow.set_data(None)
-        ow.handleNewSignals()
-        ow.onDeleteWidget()
-        return 0
+        n_epochs=400
+        lr=1e-3
+        use_batches=False
+        use_cuda=True
 
-    if __name__ == "__main__":
-        sys.exit(main(sys.argv))
+        vae = VAE(dataset.nb_genes, n_batch=dataset.n_batches * use_batches)
+        trainer = UnsupervisedTrainer(vae, dataset ,train_size=0.75, use_cuda=use_cuda ,frequency=5)
+        trainer.train(n_epochs=n_epochs, lr=lr)
+
+
+        ll_train_set = trainer.history["ll_train_set"]
+        ll_test_set = trainer.history["ll_test_set"]
+        x = numpy.linspace(0,500,(len(ll_train_set)))
+        plt.plot(x, ll_train_set)
+        plt.plot(x, ll_test_set)
+        plt.ylim(1150,1600)
+        plt.show()
+
+        trainer.train_set.show_t_sne(n_samples=400, color_by='labels')
+        return
+
+    def  scvis(self):
+        return
+
+
+
+
+
+def main(argv=None):
+    from AnyQt.QtWidgets import QApplication
+    # PyQt changes argv list in-place
+    app = QApplication(list(argv) if argv else [])
+    argv = app.arguments()
+    if len(argv) > 1:
+        filename = argv[1]
+    else:
+        filename = "./data/_sc_aml-1k.csv"
+
+    ow = Impute(filename)
+    ow.show()
+    ow.raise_()
+
+    dataset = Orange.data.Table(filename)
+
+    ow.set_data(dataset)
+    ow.handleNewSignals()
+    app.exec_()
+    ow.set_data(None)
+    ow.handleNewSignals()
+    ow.onDeleteWidget()
+    call(["python", "owtable.py"])
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
